8fee6946cb32cac678c9d0da4b05711058a1c182
Shaza Elmorshidy <shaza_1998@hotmail.com>
2024-06-24 17:52:27 +0300

    remove un-needed inline comments

--- main.py
+++ main.py
@@ -18,21 +18,17 @@ def summarize_text(text):
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # Initialize the summarization pipeline
     summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
     # summarizer = pipeline("summarization")
     summary = summarizer(text, max_length=350, min_length=30, do_sample=False)
     return summary[0]['summary_text']
 
 def main(image_path):
-    # Preprocess the image
     preprocessed_image = preprocess_pipeline(image_path)
     
-    # Perform OCR to extract text
     extracted_text = ocr_image(preprocessed_image)
     print("Extracted Text:", extracted_text)
     
-    # Summarize the extracted text
     summary = summarize_text(extracted_text)
     print("Summary:", summary)
     
